fc4f5eb12ebe18e3242eda150875bd9933a41dd6
Vb1221 <vb259550@gmail.com>
2023-06-19 23:57:37 +0300

    fix

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 require('dotenv').config();
 
-const token = process.env.BOT_TOKEN; 
+const token = process.env.BOT_TOKEN;
 
 const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(token, { polling: true });
 
-function splitStrWithDelay(msg) {
+async function splitStrWithDelay(msg) {
   const chatId = msg.chat.id;
   const messageText = msg.text;
   const msgs = messageText.split('\n');
 
-  let i = 0;
-  const sendMessageWithDelay = setInterval(() => {
-    if (i >= msgs.length) {
-      clearInterval(sendMessageWithDelay);
-      return;
-    }
-
-    bot.sendMessage(chatId, msgs[i]);
-    i++;
-  }, 500);
+  for (let i = 0; i < msgs.length; i++) {
+    await bot.sendMessage(chatId, msgs[i]);
+  }
 }
 
 bot.on('message', (msg) => {
