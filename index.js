6712eb4fa22797f1693c3e78ad7b1793be5eabbe
Vb1221 <vb259550@gmail.com>
2023-06-20 02:34:54 +0300

    fix

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(token, { polling: true });
 
-
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
   splitStrWithDelay(messageText, chatId);
 });
 
-async function splitStrWithDelay(messageText, chatId) {
+function splitStrWithDelay(messageText, chatId) {
 
   const msgs = messageText.split('\n');
 
-  for (let i = 0; i < msgs.length; i++) {
-    await bot.sendMessage(chatId, msgs[i]);
-  }
+  msgs.forEach((message) => {
+    bot.sendMessage(chatId, message);
+  });
+  
 }
 
 console.log("work")
