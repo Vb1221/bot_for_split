f37071d2e58e2737bd3394ab587eb1c454ed44ac
Vb1221 <vb259550@gmail.com>
2023-06-20 17:40:34 +0300

    test deploy

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
 
 const token = process.env.BOT_TOKEN;
-
-const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
-  splitStrWithDelay(messageText, chatId);
-});
-
-function splitStrWithDelay(messageText, chatId) {
-
   const msgs = messageText.split('\n');
-
   msgs.forEach((message) => {
     bot.sendMessage(chatId, message);
   });
-  
-}
+});
+
 
 console.log("work")
